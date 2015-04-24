d0aa6680b6e96ef6dd8adef0bc72f560e1309827
Stijn Kuipers <stijnkuipers@gmail.com>
2015-04-24 14:00:32 +0200

    adding portopening

--- classes/arduino.js
+++ classes/arduino.js
@@ -8,33 +8,31 @@ define(function(require, exports, module){
 	{
 		var serialPortModule = define.serialOnce || (define.serialOnce = require("serialport"));
 		var SerialPort = serialPortModule.SerialPort
-
-		function readline (delimiter, encoding) 
-		{
-			if (typeof delimiter === 'undefined' || delimiter === null) { delimiter = '\r'; }
-			if (typeof encoding  === 'undefined' || encoding  === null) { encoding  = 'utf8'; }
-			// Delimiter buffer saved in closure
-			var data = '';
-			return function (emitter, buffer) 
-			{
-				data += buffer.toString(encoding);
-				// Split collected data by delimiter
-				var parts = data.split(delimiter);
-				data = parts.pop();
-				parts.forEach(function (part) 
-				{
-					emitter.emit('data', part);
-				});
-			};
-		}  
-	  
-		
-
 	}
 	catch(e){
 
 	}
-	
+
+	function lineConsumer (delimiter, encoding) 
+	{
+		if (typeof delimiter === 'undefined' || delimiter === null) { delimiter = '\r'; }
+		if (typeof encoding  === 'undefined' || encoding  === null) { encoding  = 'utf8'; }
+		// Delimiter buffer saved in closure
+		var data = '';
+		return function (emitter, buffer) 
+		{
+			data += buffer.toString(encoding);
+			// Split collected data by delimiter
+			var parts = data.split(delimiter);
+			data = parts.pop();
+			parts.forEach(function (part) 
+			{
+				emitter.emit('data', part);
+			});
+		};
+	}  
+
+		
 	return node.extend("arduino", function(){
 	
 		this.portOpened = false;
@@ -47,38 +45,45 @@ define(function(require, exports, module){
 		{
 			console.color('~br~Arduino~~ object started on server\n')	
 			console.log("my port: " + this.port);
+			this.openPort = function()
+			{
+				console.log(this.portOpened);
+			}.bind(this);
+			
 			this.scanPorts = function()
 			{
-			var RootThis = this;
-			var P = this.port;
-			var PS = this.portScanner;
-			serialPortModule.list(function (err, ports) 
-			{
-				ports.forEach(function(port) 
+				var RootThis = this;
+				var P = this.port;
+				var PS = this.portScanner;
+				serialPortModule.list(function (err, ports) 
 				{
-				//	console.log("available com port: " + port.comName + " " + port.pnpId + " " + port.manufacturer);
-					//console.log(this.port);
-					if (P != undefined)
+					ports.forEach(function(port) 
 					{
-						if (port.comName.toLowerCase() == P.toLowerCase())
+					//	console.log("available com port: " + port.comName + " " + port.pnpId + " " + port.manufacturer);
+						//console.log(this.port);
+						if (P != undefined)
 						{
-							console.log("found "+ P + "! Connecting!");
-							this.clearInterval(PS);
-							RootThis.portScanner = undefined;
-							
+							if (port.comName.toLowerCase() == P.toLowerCase())
+							{
+								console.log("found "+ P + "! Connecting!");
+								this.clearInterval(PS);
+								RootThis.portScanner = undefined;		
+								RootThis.openPort();										
+							}
 						}
-	
-					}
+					});
 				});
-			});
-
-		}.bind(this);
+			}.bind(this);
 			
 			this.portScanner = setInterval(this.scanPorts, 1000);
 		}
-		
-		this.test = function(){
-		console.log('test called!')
-	}
+		this.destroy = function()
+		{
+			
+		}
+		this.test = function()
+		{
+			console.log('test called!')
+		}
 	})
 })
