fff3651b0827fe07e7d4763fc934f6ea950908fc
Rik Arends <rik@n4.io>
2015-04-25 11:21:00 +0900

    sprite

---
+++ classes/sprite.js
@@ -0,0 +1,32 @@
+/*
+ The MIT License (see LICENSE)
+ Copyright (C) 2014-2015 Teem2 LLC
+*/
+
+define(function(require, exports, module){
+	var node = require("../classes/node")
+
+	if(define.env == 'v8'){
+		return node.extend("sprite", function(){
+			this.spawn = function(parent){
+			}
+		})
+	}
+	else if(define.env == 'browser'){
+		return node.extend("sprite", function(){
+
+			this.render = function(){
+				return this
+			}
+
+			this.spawn = function(parent){
+				this.domNode = document.createElement('div')
+				parent.domNode.appendChild(this.domNode)
+			}
+		})
+	}
+	else{
+		return node.extend("sprite")
+	}
+
+})
