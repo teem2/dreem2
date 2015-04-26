236c94f43a1f13d2f1774af2457b114f6693f684
Rik Arends <rik@n4.io>
2015-04-27 08:51:01 +0900

    missing

---
+++ classes/server.js
@@ -0,0 +1,12 @@
+/*
+ The MIT License (see LICENSE)
+ Copyright (C) 2014-2015 Teem2 LLC
+*/
+
+define(function(require, exports, module){
+	var node = require("$CLASSES/node")
+	return node.extend("server", function(){
+		this.construct = function(){
+		}
+	})
+})
