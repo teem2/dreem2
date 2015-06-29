532f6f58fd9512c3e487215cc40331623b25391e
Raju Bitter <rajubitter@gmail.com>
2015-06-29 12:30:22 +0200

    Fix problem with Dali application processed not getting killed properly (results in multiple Dali application windows open at the same time).

--- core/daliclient.js
+++ core/daliclient.js
@@ -1,7 +1,7 @@
 /*
  The MIT License (see LICENSE)
  Copyright (C) 2014-2015 Teem2 LLC
-*/
+ */
 /**
  * @class DaliGen
  * Class to create dali JS app (testing)
@@ -12,7 +12,8 @@ define(function(require, exports, module) {
   var http = require('http'),
     url = require('url'),
     fs = require('fs'),
-    child_process = require('child_process'),
+    cp = require('child_process'),
+    forever = require('forever-monitor'),
     NodeWebSocket = require('./nodewebsocket');
 
   // lets monitor all our dependencies and terminate if they change
@@ -24,73 +25,120 @@ define(function(require, exports, module) {
     this.reconnect();
   };
 
+  function killDaliProcessTree(pid, killTree, signal, callback) {
+    signal   = signal   || 'SIGKILL';
+    callback = callback || function () {};
+
+    if (killTree && process.platform !== 'win32') {
+      psTree(pid, function (err, children) {
+        [pid].concat(
+          children.map(function (p) {
+            return p.PID;
+          })
+        ).forEach(function (tpid) {
+            try { process.kill(tpid, signal) }
+            catch (ex) { }
+          });
+
+        callback();
+      });
+    }
+    else {
+      try { process.kill(pid, signal) }
+      catch (ex) { }
+      callback();
+    }
+  }
+
   body.call(DaliClient.prototype);
 
   function body() {
     // connect to server
     this.redownload = function() {
-					console.log('starting redownload...');
+      console.log('starting redownload...');
 
       // lets fetch the main thing
       http.get({
-        host: this.url.hostname,
-        port: this.url.port,
-        path: this.url.path
-      }, 
-      function(res) {
-        var data = '';
-        res.on('data', function(buf) {data += buf;});
-        res.on('end', function() {
-			console.log('got data!');
+          host: this.url.hostname,
+          port: this.url.port,
+          path: this.url.path
+        },
+        function(res) {
+          var data = '';
+          res.on('data', function(buf) {
+            data += buf;
+          });
+          res.on('end', function() {
+            var that = this;
+            that.isRestarting = false;
+            console.log('Dreem application updated, writing dali.js!');
+            // write it and restart it.
+            try {
+              fs.writeFileSync('./dali.js', data);
+              if (this.child) {
+                  if (that.curProcId != -1) {
+                    console.log("Stopping Dali application with PID " + that.curProcId);
+                    process.kill(that.curProcId);
+                    that.curProcId = -1;
+                  }
 
-          // write it and restart it.
-          try {
-            console.log('writing dali.js!');
-			fs.writeFileSync('./dali.js', data);
-            if (this.child) {
-            console.log('child process already running - removing it first.');
-			  var kill = this.child;
-              this.child = undefined;
-              var i = 0;
-              var itv = setInterval(function() {
-                try {kill.kill('SIGTERM');console.log('attempting to stop process');} catch(e) {}
-                if (i++ > 20) clearInterval(itv);
-              },10);
+              } else {
+                this.child = forever.start([ './scriptrunner.example', './dali.js' ], {
+                  silent: true,
+                  'uid': 'dreemdali',
+                  'minUptime': 3000,     // Minimum time a child process has to be up. Forever will 'exit' otherwise.
+                  'spinSleepTime': 1000, // Interval between restarts if a child is spinning (i.e. alive < minUptime).
+                  // 'sourceDir': 'script/path',// Directory that the source script is in
+                });
+                console.log('Spawning forever-monitor instance for Dali: pid=', this.child.child.pid);
+                this.child.on('start', function() {
+                  that.curProcId = that.child.child.pid;
+                  console.log("forever process started: curProcId=" + that.curProcId);
+                });
+                this.child.on('restart', function() {
+                  that.curProcId = that.child.child.pid;
+                  console.log("forever process started: curProcId=" + that.curProcId);
+                });
+                this.child.on('exit', function() {
+                  console.log("forever process with PID " + that.curProcId + " exited");
+                });
+              }
+
+
+              /* this.child = child_process.spawn('./scriptrunner.example', ['./dali.js']);
+               this.child.on('close', function(code) {
+               this.child = undefined;
+               }.bind(this));
+               this.child.on('error', function(){
+               });*/
+            } catch(e) {
+              console.log(e)
             }
-            console.log('spawning dali process!');
-            this.child = child_process.spawn('./scriptrunner.example', ['./dali.js']);
-            this.child.on('close', function(code) {
-              this.child = undefined;
-            }.bind(this));
-            this.child.on('error', function(){});
-          } catch(e) {
-            //console.log(e)
-          }
+          }.bind(this));
         }.bind(this));
-      }.bind(this));
     }
-    
+
     this.reconnect = function() {
       // put up websocket.
       if (this.sock) this.sock.close();
-      
+
       this.sock = new NodeWebSocket(this.host);
       this.sock.onError = function(msg) {
         setTimeout(function() {
           this.reconnect();
         }.bind(this), 500);
       }.bind(this);
-      
+
       this.sock.onMessage = function(msg) {
         try {
           msg = JSON.parse(msg);
         } catch(e){}
         if (msg.type == "sessionCheck") {
-			console.log('attempting redownload!');
+          console.log('attempting redownload!');
           this.redownload();
         }
       }.bind(this);
-      
+
       this.sock.onClose = function() {
         setTimeout(function() {
           this.reconnect();
