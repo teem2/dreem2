6f4b23e34c5a43493879353e6e1753a62b73be8e
Maynard Demmon <maynarddemmon@gmail.com>
2015-08-29 13:19:11 -0700

    Fix a naked variable. Fix regression in dreemcompiler when I made better method names.

--- core/dreemcompiler.js
+++ core/dreemcompiler.js
@@ -90,7 +90,7 @@ define(function(require, exports, module) {
     }
     
     // give the method a unique but human readable name
-    var name = node.tag + '_' + (node.attr && node.attr.name || node.attr.event) + '_' + node.pos + '_' + language;
+    var name = node.tag + '_' + (node.attr && (node.attr.name || node.attr.event)) + '_' + node.pos + '_' + language;
     if (parent && (parent.tag == 'class' || parent.tag == 'mixin')) name = (parent.attr && parent.attr.name) + '_' + name;
     name = name.split(define.SEPARATOR_REGEX).join('_');
     
